6315239d2b030c5b5efb04b5bea3fd25de9613d1
Kevin Louis <kevin.santos@ateliware.com.br>
2019-08-11 20:13:19 -0300

    added match feature on mobile

--- app/src/index.js
+++ app/src/index.js
@@ -7,11 +7,11 @@
  */
 
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
+import {YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection option(s)'
+])
 
 import Routes from './routes'
 
--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React , { useState, useEffect } from 'react'
+import io from 'socket.io-client'
 import AsyncStorage from '@react-native-community/async-storage'
 import {SafeAreaView, View, Text, Image, StyleSheet} from 'react-native'
 
@@ -7,11 +8,13 @@ import axios from 'axios'
 import logo from '../assets/logo.png'
 import like from '../assets/like.png'
 import dislike from '../assets/dislike.png'
+import itsamatch from '../assets/itsamatch.png'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Main({ navigation }){
     const id = navigation.getParam('user')
     const [users, setUsers] = useState([])
+    const [matchDev, setMatchDev] = useState(null)
 
     useEffect(() => {
         async function loadUsers(){
@@ -22,6 +25,18 @@ export default function Main({ navigation }){
         }
 
         loadUsers()
+    }, [id])
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: {
+                user: id
+            }
+        })
+
+        socket.on('match', dev => {
+            setMatchDev(dev)
+        })
     }, [id])
 
     async function handleLike(){
@@ -84,6 +99,19 @@ export default function Main({ navigation }){
                     </TouchableOpacity>
                 </View>
             ) : <View />}
+
+            { matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image source={itsamatch} />
+                    <Image style={styles.matchAvatar} source={{uri: matchDev.avatar}} />
+
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.closeMatch}>FECHAR</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
         </SafeAreaView>
     );
 }
@@ -172,5 +200,49 @@ const styles = StyleSheet.create({
         color: '#999',
         fontSize: 24,
         fontWeight: 'bold'  
+    },
+
+    matchContainer: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0, 0.8)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain'
+    },
+
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        marginVertical: 30
+    },
+
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    
+    matchBio: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'rgba(255, 255, 255, 0.8)',
+        lineHeight: 24,
+        textAlign: 'center',
+        paddingHorizontal: 30,
+    },
+
+    closeMatch: {
+        fontSize: 16, 
+        color: 'rgba(255, 255, 255, 0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold'
     }
 })
